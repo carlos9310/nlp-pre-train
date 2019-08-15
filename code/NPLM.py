cea7b444952caa26c2092882725a53dc5d018402
carlos9310 <15356059310@163.com>
2019-08-15 22:35:03 +0800

    update nplm.py

--- code/NPLM.py
+++ code/NPLM.py
@@ -1,26 +1,26 @@
-import os
 import time
 import math
 import numpy as np
 import tensorflow as tf
 from collections import Counter
 
-# hyper parameters
-dir_corpus = 'data'
-sizeof_batch = 120
-window_size = 5
-hidden_l_sz = 100
-word_dim = 50
-num_epochs = 1
-grad_clip = 10
-vocab_size = 10000
-alpha = 0.01
-file_name = 'text8'  # input file name
+flags = tf.flags
+FLAGS = flags.FLAGS
+
+# parameters 
+flags.DEFINE_string('path_corpus', '../data/text8', 'the path of the corpus file')
+flags.DEFINE_integer('train_batch_size', 120, 'number of words in a batch for train')
+flags.DEFINE_integer('vocab_size', 10000, '词表中单词数的上限')
+flags.DEFINE_integer('window_size', 5, 'n-gram(前n-1个词)')
+flags.DEFINE_integer('num_epochs', 1, 'num_epochs')
+flags.DEFINE_integer('embedding_dim', 50, '词向量的维度')
+flags.DEFINE_integer('hidden_size', 100, '隐层神经元的个数')
+flags.DEFINE_float('learning_rate', 0.01, 'learning_rate')
 
 
+# data pre-process
 class CorpusProcess:
-    def __init__(self, data_dir, batch_size, seq_length, corpus_name, dict_size):
-        self.data_dir = data_dir
+    def __init__(self, batch_size, seq_length, dict_size):
         self.batch_size = batch_size
         self.context_window_size = seq_length
         self.batch_ptr = 0
@@ -29,8 +29,7 @@ class CorpusProcess:
         self.data = []
         self.reverse_dic_wrd = {}
         self.num_batches = 0
-        input_file = os.path.join(data_dir, corpus_name)
-        self.pre_process(input_file)
+        self.pre_process(FLAGS.path_corpus)
 
     # data pre_processing
     def pre_process(self, input_file):
@@ -42,7 +41,7 @@ class CorpusProcess:
 
         count = [['UNK', -1]]
         frq_word = Counter(words)
-        frq_word = frq_word.most_common(vocab_size - 1)
+        frq_word = frq_word.most_common(FLAGS.vocab_size - 1)
         count.extend(frq_word)
 
         for word, _ in count:
@@ -55,16 +54,15 @@ class CorpusProcess:
                 idx = self.dic_wrd[word]
             self.data.append(idx)
         print('dict created')
-        self.num_batches = int(len(self.data) / self.batch_size)
+        self.num_batches = int((len(self.data) - 1) / self.batch_size) + 1
         # words
         self.reverse_dic_wrd = dict(zip(self.dic_wrd.values(), self.dic_wrd.keys()))
 
     # to get next batch
-    def next_batch(self):
-
-        if self.num_batches == 0:
-            assert False, 'number of batches is 0.'
-        batch_data = self.data[self.batch_ptr:self.batch_ptr + self.batch_size]
+    def next_batch(self): 
+        start_idx = self.batch_ptr
+        end_idx = min(self.batch_ptr + self.batch_size, len(self.data))
+        batch_data = self.data[start_idx:end_idx]
         self.batch_ptr = (self.batch_ptr + 1) % self.num_batches
         x_data, y_data = [], []
         for i in range(len(batch_data) - self.context_window_size):
@@ -74,68 +72,67 @@ class CorpusProcess:
 
 
 # calling for pre_processing
-my_data = CorpusProcess(dir_corpus, sizeof_batch, window_size, file_name, vocab_size)
-vocab_size = my_data.vocab_size
+corpus = CorpusProcess(FLAGS.train_batch_size, FLAGS.window_size, FLAGS.vocab_size)
+vocab_size = corpus.vocab_size
 
-# setting up the graph and the computational unit for tensorflow
+# building the computational graph for nplm
 graph = tf.Graph()
 with graph.as_default():
     # creating placeholder
-    tf_in_data = tf.placeholder(tf.int32, [sizeof_batch-5, window_size])
-    targets = tf.placeholder(tf.int64, [sizeof_batch-5, 1])
+    input_data = tf.compat.v1.placeholder(tf.int32, [FLAGS.train_batch_size-FLAGS.window_size, FLAGS.window_size])
+    targets = tf.compat.v1.placeholder(tf.int64, [FLAGS.train_batch_size-FLAGS.window_size, 1])
 
     # creating the variable scope for embeds
-    with tf.variable_scope('nplm_embed'):
-        embeds = tf.Variable(tf.random_uniform([vocab_size, word_dim], -1.0, 1.0))
+    with tf.compat.v1.variable_scope('nplm_embed'):
+        embeds = tf.Variable(tf.random.uniform([vocab_size, FLAGS.embedding_dim], -1.0, 1.0))
         embeds = tf.nn.l2_normalize(embeds, 1)
 
-    with tf.variable_scope('nplm_weight'):
+    with tf.compat.v1.variable_scope('nplm_weight'):
         # weight matrix variable for word embedding to hidden layer
         # this variable is initialized with random values
-        weight_h = tf.Variable(tf.truncated_normal([window_size * word_dim + 1, hidden_l_sz],
-                                                   stddev=1.0 / math.sqrt(hidden_l_sz)))
+        weight_h = tf.Variable(tf.random.truncated_normal([FLAGS.window_size * FLAGS.embedding_dim + 1, FLAGS.hidden_size],
+                                                          stddev=1.0 / math.sqrt(FLAGS.hidden_size)))
         # softmax variable
-        softmax_w = tf.Variable(tf.truncated_normal([window_size * word_dim, vocab_size],
-                                                    stddev=1.0 / math.sqrt(window_size * word_dim)))
-        softmax_u = tf.Variable(tf.truncated_normal([hidden_l_sz + 1, vocab_size],
-                                                    stddev=1.0 / math.sqrt(hidden_l_sz)))
+        softmax_w = tf.Variable(tf.random.truncated_normal([FLAGS.window_size * FLAGS.embedding_dim, vocab_size],
+                                                           stddev=1.0 / math.sqrt(FLAGS.window_size * FLAGS.embedding_dim)))
+        softmax_u = tf.Variable(tf.random.truncated_normal([FLAGS.hidden_size + 1, vocab_size],
+                                                           stddev=1.0 / math.sqrt(FLAGS.hidden_size)))
     # lookup of nn  embedding and the with data input data
-    in_emb = tf.nn.embedding_lookup(embeds, tf_in_data)
-    in_emb = tf.reshape(in_emb, [-1, window_size * word_dim])
-    tf_stack = tf.stack([tf.shape(tf_in_data)[0], 1])
-    tf_ones = tf.ones(tf_stack)
+    in_emb = tf.nn.embedding_lookup(embeds, input_data)
+    in_emb = tf.reshape(in_emb, [-1, FLAGS.window_size * FLAGS.embedding_dim])
+    # add bias item
+    tf_ones = tf.ones([tf.shape(input_data)[0], 1])
     in_emb_add = tf.concat([in_emb, tf_ones], 1)
     # tanh activation function
     inputs = tf.tanh(tf.matmul(in_emb_add, weight_h))
-    inputs_add = tf.concat([inputs, tf.ones(tf.stack([tf.shape(tf_in_data)[0], 1]))], 1)
+    inputs_add = tf.concat([inputs, tf.ones([FLAGS.train_batch_size-FLAGS.window_size, 1])], 1)
     # matmul for output
     opts = tf.matmul(inputs_add, softmax_u) + tf.matmul(in_emb, softmax_w)
-    opts = tf.clip_by_value(opts, 0.0, grad_clip)
-    # applying softmax
-    opts = tf.nn.softmax(opts)
-    onehot_tar = tf.one_hot(tf.squeeze(targets), vocab_size, 1.0, 0.0)
     # loss function and optimizer
-    loss = -tf.reduce_mean(tf.reduce_sum(tf.log(opts) * onehot_tar, 1))
-    optimizer = tf.train.AdagradOptimizer(alpha).minimize(loss)
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=opts, labels=tf.squeeze(targets)))
+    optimizer = tf.compat.v1.train.AdagradOptimizer(FLAGS.learning_rate).minimize(loss)
 
-    embeds_norm = tf.sqrt(tf.reduce_sum(tf.square(embeds), 1, keep_dims=True))
+    # normalized embedding
+    embeds_norm = tf.sqrt(tf.reduce_sum(tf.square(embeds), 1, keepdims=True))
     norm_embed = embeds / embeds_norm
 
 
 # running the graph in the session
-with tf.Session(graph=graph) as sess:
+with tf.compat.v1.Session(graph=graph) as sess:
     # initializing global variables
-    tf.global_variables_initializer().run()
-    # running for only one epoch
-    for b in range(my_data.num_batches):
-        start = time.time()
-        # get next batch
-        x, y = my_data.next_batch()
-        feed = {tf_in_data: x, targets: y}
-        train_loss,  _ = sess.run([loss, optimizer], feed)
-        end = time.time()
-        if b % 10000 == 0 or b == my_data.num_batches - 1:
-            print("{}/{} (epoch {}), train_loss = {:.3f}, time/batch = {:.3f}" .format(
-                    b, my_data.num_batches,
-                    num_epochs-1, train_loss, end - start))
-        np.save('wrd_embeds', norm_embed.eval())
+    tf.compat.v1.global_variables_initializer().run()
+    for num_epoch in range(FLAGS.num_epochs):
+        for b in range(corpus.num_batches):
+            start = time.time()
+            # get next batch
+            x, y = corpus.next_batch()
+            feed = {input_data: x, targets: y}
+            # training for each batch
+            train_loss,  _ = sess.run([loss, optimizer], feed)
+            end = time.time()
+            if b % 10 == 0 or b == corpus.num_batches - 1:
+                print("{}/{} (epoch {}), train_loss = {:.3f}, time/batch = {:.3f}" .format(
+                        b, corpus.num_batches, num_epoch + 1, train_loss, end - start))
+    # word embedding
+    np.save('wrd_embeds', norm_embed.eval())
+
--- data/readme.md
+++ data/readme.md
@@ -1,1 +1,5 @@
 the corpus for pre-training language model
+
+command
+
+- unzip text8.zip -d data
